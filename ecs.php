92f5dc8dc501e1bb5159f9ace90b3ce48369be70
timoz <mozelle.remy@hotmail.fr>
2026-07-31 16:34:18 +0400

    feature(): Add ECS config with custom ddev ecs command

---
+++ ecs.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withEditorConfig()
+    ->withRootFiles()
+    ->withPreparedSets(
+        psr12: true,
+        common: true,
+        cleanCode: true
+    );
